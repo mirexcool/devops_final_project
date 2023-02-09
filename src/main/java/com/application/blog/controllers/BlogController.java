f6afce6b79bd5ec38b321970039242e9b1c07281
mirexcool <mirexcool@gmail.com>
2023-02-09 21:26:14 +0200

    fix exeption

--- src/main/java/com/application/blog/controllers/BlogController.java
+++ src/main/java/com/application/blog/controllers/BlogController.java
@@ -77,29 +77,21 @@ public class BlogController {
                               @RequestParam String anons,
                               @RequestParam String full_text,
                               Model model){
-        try {
-            Post post = postRepository.findById(id).orElseThrow();
-            post.setTitle(title);
-            post.setAnons(anons);
-            post.setFullText(full_text);
-            postRepository.save(post);
-        }catch (Exception e){
+        Post post = postRepository.findById(id).orElseThrow();
 
-        }
-
-
+        post.setTitle(title);
+        post.setAnons(anons);
+        post.setFullText(full_text);
+        postRepository.save(post);
         return "redirect:/blog";
     }
 
     @PostMapping("/blog/{id}/remove")
     public String blogPostDelete(@PathVariable(value = "id") long id,
                                  Model model){
-        try {
-            Post post = postRepository.findById(id).orElseThrow();
-            postRepository.delete(post);
-        }catch (Exception e){
+        Post post = postRepository.findById(id).orElseThrow();
 
-        }
+        postRepository.delete(post);
         return "redirect:/blog";
     }
 }
